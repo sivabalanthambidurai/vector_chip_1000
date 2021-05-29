c47e69bc2cfc2ecc17616c908a4921c8eea5bde0
sivabalanthambidurai <63663656+sivabalanthambidurai@users.noreply.github.com>
2021-05-29 19:41:33 +0530

    adding functional and load/store logic

---
+++ hw/rtl/vector_functional_unit.sv
@@ -0,0 +1,14 @@
+//____________________________________________________________________________________________________________________
+//file name : vector_functional_unit.sv
+//author : sivabalan
+//description : This file holds the vector functional logic. Vector functional unit holds the following functions
+//TODO: 
+//____________________________________________________________________________________________________________________
+
+module vector_functional_unit (input clk,
+                               input reset
+
+                               );
+  
+  
+endmodule
---
+++ hw/rtl/vector_load_store_unit.sv
@@ -0,0 +1,15 @@
+//____________________________________________________________________________________________________________________
+//file name : vector_load_store_unit.sv
+//author : sivabalan
+//description : This file holds the vector load and store unit. This module is responsible for generating memory read and 
+//write request.  
+//____________________________________________________________________________________________________________________
+
+
+module vector_load_store_unit (input clk,
+                               input reset
+
+                               );
+  
+  
+endmodule
