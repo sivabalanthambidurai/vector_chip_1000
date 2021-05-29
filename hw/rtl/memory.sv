ee9e01586e64c4ed066a7a4a3046ea71ffb9248e
sivabalanthambidurai <63663656+sivabalanthambidurai@users.noreply.github.com>
2021-05-30 00:18:40 +0530

    memory logic update

---
+++ hw/rtl/memory.sv
@@ -0,0 +1,39 @@
+//____________________________________________________________________________________________________________________
+//file name : memory.sv
+//author : sivabalan
+//description : This file holds the memory.
+//____________________________________________________________________________________________________________________
+
+module memory (input clk,
+               input write,
+               input [(DATA_FIELD_WIDTH/BYTE)-1:0] we,
+               input [ADDR_FIELD_WIDTH-1:0] addr,
+               input [DATA_FIELD_WIDTH-1:0] data,
+                              
+               output [DATA_FIELD_WIDTH-1:0] q
+               );
+  
+  logic [DATA_FIELD_WIDTH-1:0] memory [1<<ADDR_FIELD_WIDTH-1];
+  logic [ADDR_FIELD_WIDTH-1:0] addr_ff;
+  
+  always_ff@(posedge clk)
+  begin
+     if(write)
+     begin
+        foreach(we[i])
+        begin
+           if(we[i])
+           begin
+              memory[addr][8*i +: 8] <= data[8*i +: 8];
+           end
+        end
+     end
+     else
+     begin
+        addr_ff <= addr;
+     end
+  end
+
+assign q = memory[addr_ff];
+ 
+endmodule
--- hw/rtl/parameters.sv
+++ hw/rtl/parameters.sv
@@ -4,7 +4,13 @@
 //description : This file include the predefined parameter values.
 //____________________________________________________________________________________________________________________
 
+//registers
 parameter VECTOR_REG_DEPTH = 64; 
 parameter VECTOR_REG_WIDTH = 64;
 parameter SCALAR_REG_DEPTH = 32; 
 parameter SCALAR_REG_WIDTH = 64;
+
+//memory
+parameter BYTE = 8;
+parameter ADDR_FIELD_WIDTH = 16;
+parameter DATA_FIELD_WIDTH = 64;
