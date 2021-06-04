ce764aad78b284ca189df2df48f2c4e851196c35
sivabalanthambidurai <63663656+sivabalanthambidurai@users.noreply.github.com>
2021-06-04 17:47:24 +0530

    added core module

---
+++ hw/rtl/core.sv
@@ -0,0 +1,16 @@
+//____________________________________________________________________________________________________________________
+//file name : core.sv
+//author : sivabalan
+//description : This file holds the top level core logic.
+//____________________________________________________________________________________________________________________
+
+module memory_controller (input clk,
+                          input reset,
+                    
+                          //memory interface
+                          input request_t mem_rsp,
+                          output request_t mem_req
+                       
+                          );
+  
+endmodule
