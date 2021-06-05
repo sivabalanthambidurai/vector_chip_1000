4874496a2df6223f17d8d4be775a2e500a5530a4
sivabalanthambidurai <63663656+sivabalanthambidurai@users.noreply.github.com>
2021-06-05 10:48:03 +0530

    added load/store request generation logic

--- hw/rtl/parameters.sv
+++ hw/rtl/parameters.sv
@@ -12,6 +12,12 @@ parameter VECTOR_REG_DEPTH = 64;
 parameter VECTOR_REG_WIDTH = 64;
 parameter SCALAR_REG_DEPTH = 32; 
 parameter SCALAR_REG_WIDTH = 64;
+parameter NUM_OF_VECTOR_REG = 8;
+
+//load and store
+parameter LOAD_STORE_BUFFER_DEPTH = 64;
+parameter REQUEST_COUNTER_WIDTH = 8;
+parameter REQUEST_LENGHT_WIDTH = 8;
 
 //memory
 parameter BYTE = 8;
@@ -23,6 +29,7 @@ parameter ACCESS_ID_WIDTH = 8;
 parameter CORE_ID_WIDTH = 4;
 
 typedef enum { NULL_ACCESS, READ_REQ, WRITE_REQ, READ_RSP, WRITE_RSP} access_type_t;
+typedef enum { NON_STRIDE, STRIDE, INDEX} stride_type_t;
 
 typedef struct packed {
     bit vld;
@@ -33,6 +40,15 @@ typedef struct packed {
     bit [(DATA_FIELD_WIDTH/BYTE)-1:0] byte_en;
     bit [VECTOR_REG_DEPTH-1:0] data;
 } request_t;
+
+typedef struct packed {
+    bit vld;
+    access_type_t access_type;
+    bit [REQUEST_LENGHT_WIDTH-1:0] access_length;
+    stride_type_t stride_type;
+    bit [$clog2(NUM_OF_VECTOR_REG)-1:0] vec_reg_ptr;
+    bit [ADDR_FIELD_WIDTH-1:0] addr;
+} cntrl_req_t;
 
 `define flip_flop(clk, reset, in, out)\
 always_ff@(posedge clk or negedge reset)\
--- hw/rtl/vector_load_store_unit.sv
+++ hw/rtl/vector_load_store_unit.sv
@@ -7,9 +7,73 @@
 
 
 module vector_load_store_unit (input clk,
-                               input reset
+                               input reset,
 
+                               //execution unit interface
+                               input cntrl_req_t cntrl_req,
+                               output reg buffer_full,
+
+                               //memory request interface
+                               input request_t mem_rsp,
+                               output reg rsp_rcvd,
+                               input req_grant,
+                               output request_t mem_req
                                );
   
+  //load and store buffer. Can supppot only one vector load or store with maximum size of 64.
+  logic [REQUEST_COUNTER_WIDTH-1:0] request_count, req_sent_count, rsp_rcvd_count;
+  cntrl_req_t cntrl_req_ff;
+
+
+  always_ff @(posedge clk or negedge reset) begin
+     if(!reset)
+        cntrl_req_ff <= 'h0;
+     else if(cntrl_req.vld && !buffer_full)
+        cntrl_req_ff <= cntrl_req;
+  end
   
+  always_ff @(posedge clk or negedge reset) begin
+     if(!reset)
+        request_count <= 'h0;
+     else if(cntrl_req.vld && !buffer_full) 
+        request_count <= cntrl_req.access_length;
+  end
+
+  always_ff @(posedge clk or negedge reset) begin
+     if(!reset)
+        req_sent_count <= 'h0;
+     else if(cntrl_req.vld && !buffer_full)
+        req_sent_count <= 'h0;
+     else if(mem_req.vld)
+        req_sent_count <= req_sent_count + 1;
+  end
+
+  always_ff @(posedge clk or negedge reset) begin
+     if(!reset)
+        rsp_rcvd_count <= 'h0;
+     else if(cntrl_req.vld && !buffer_full)
+        rsp_rcvd_count <= cntrl_req.access_length;
+     else if(mem_rsp.vld)
+        rsp_rcvd_count <= rsp_rcvd_count + 1;
+  end
+
+  always_ff @(posedge clk or negedge reset) begin
+     if(!reset)
+        mem_req <= 'h0;
+     else if((req_sent_count < request_count) && (!mem_req.vld || (mem_req.vld && req_grant))) begin
+        mem_req.vld <= 1;
+        mem_req.access_type <= cntrl_req.access_type;
+        mem_req.access_id <= (req_sent_count == 0) ? 0 : mem_req.access_id + 1;
+        //mem_req.core_id <= 0 TODO : FIXME
+        mem_req.addr <= cntrl_req.addr + 8*mem_req.access_id;
+        mem_req.byte_en <= 'hff;
+        //mem_req.data <= 0 TODO : FIXME
+     end
+     else if(req_sent_count == request_count)
+        mem_req <= 'h0;
+  end
+
+
+assign buffer_full = (request_count != rsp_rcvd_count) ? 1 : 0;
+
 endmodule
