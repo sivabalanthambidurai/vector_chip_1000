dd4caac2eeb6740d59ca68e23a1a7eb9fae15c66
Sathish Kumar S <443sathishkumar@gmail.com>
2021-06-05 18:58:11 +0530

    Initial commit for sw

---
+++ sw/code_logic.py
@@ -0,0 +1,7 @@
+# ____________________________________________________________________________________________________________________
+# File Name   : code logic
+# Author      : Sathish Kumar
+# Description : This file includes the program that converts high level to low level language
+# ____________________________________________________________________________________________________________________
+
+print("Vector Chip 1000")
