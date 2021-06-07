064e89d9d549de1c231aa901b27a3805b33065ac
sivabalanthambidurai <63663656+sivabalanthambidurai@users.noreply.github.com>
2021-06-07 20:07:13 +0530

    added stp, wb and lane files.

---
+++ hw/rtl/lane.sv
@@ -0,0 +1,11 @@
+//____________________________________________________________________________________________________________________
+//file name : lane.sv
+//author : sivabalan
+//description : This file holds the logic for lane inside the pipeline.
+//____________________________________________________________________________________________________________________
+
+module lane (input clk,
+             input reset
+            );
+ 
+endmodule
